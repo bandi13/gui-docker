75fe9cc1625370bae7a70fa6df4e22ea8dc32163
Andras Fekete <anfekete@silabs.com>
2018-05-30 09:52:46 -0400

    Put the VNC password file in the user's home area

--- container_startup.sh
+++ container_startup.sh
@@ -2,9 +2,9 @@
 OUR_IP=$(hostname -i)
 
 #start VNC server (Uses VNC_PASSWD Docker ENV variable)
-mkdir -p /opt/.vnc && x11vnc -storepasswd $VNC_PASSWD /opt/.vnc/passwd
+mkdir -p $HOME/.vnc && x11vnc -storepasswd $VNC_PASSWD $HOME/.vnc/passwd
 # -noxrecord is to solve a 'stack smashing' bug: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=859213
-x11vnc -rfbauth /opt/.vnc/passwd -shared -forever -usepw -create -env FD_PROG=/opt/x11vnc_entrypoint.sh -noxrecord &
+x11vnc -rfbauth $HOME/.vnc/passwd -shared -forever -usepw -create -env FD_PROG=/opt/x11vnc_entrypoint.sh -noxrecord &
 #start noVNC web server
 /opt/noVNC/utils/launch.sh --listen 5901 &
 # Start up second server to allow shared sessions
