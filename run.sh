7154fbfcb4f61728ec9d3a5186a8cd37fe877f01
Andras Fekete <anfekete@silabs.com>
2018-07-30 14:29:00 -0400

    Unexpose 5902

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t vnc-webclient . && \
-docker run --shm-size=256m -it -p 5900:5900 -p 5901:5901 -p 5902:5902 vnc-webclient
+docker run --shm-size=256m -it -p 5900:5900 -p 5901:5901 vnc-webclient
