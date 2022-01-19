081d24a03dd54da4e46a511a65ca65bd63a384c7
ciegg <80511058+ciegg@users.noreply.github.com>
2022-01-19 02:18:43 -0800

    Update container_startup.sh

--- container_startup.sh
+++ container_startup.sh
@@ -5,7 +5,7 @@ OUR_IP=$(hostname -i)
 mkdir -p $HOME/.vnc && echo "$VNC_PASSWD" | vncpasswd -f > $HOME/.vnc/passwd
 vncserver :0 -localhost no -nolisten -rfbauth $HOME/.vnc/passwd -xstartup /opt/x11vnc_entrypoint.sh
 # start noVNC web server
-/opt/noVNC/utils/novnc_proxy --listen 5901 &
+/opt/noVNC/utils/launch.sh --listen 5901 &
 
 echo -e "\n\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $OUR_IP:5900"
