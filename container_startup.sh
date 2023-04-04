9a53b98a2d9fed49368c8d8d618f75bce36f4bd3
Andras Fekete <bandi13@users.noreply.github.com>
2023-04-04 09:59:36 -0400

    Fix issue with starting/stopping container

--- container_startup.sh
+++ container_startup.sh
@@ -3,6 +3,8 @@ OUR_IP=$(hostname -i)
 
 # start VNC server (Uses VNC_PASSWD Docker ENV variable)
 mkdir -p $HOME/.vnc && echo "$VNC_PASSWD" | vncpasswd -f > $HOME/.vnc/passwd
+# Remove potential lock files created from a previously stopped session
+rm -rf /tmp/.X*
 vncserver :0 -localhost no -nolisten -rfbauth $HOME/.vnc/passwd -xstartup /opt/x11vnc_entrypoint.sh
 # start noVNC web server
 /opt/noVNC/utils/launch.sh --listen 5901 &
