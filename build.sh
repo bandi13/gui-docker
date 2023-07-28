2281ab998e953387be8e9f7115b9e8ae57401432
Andras Fekete <bandi13@users.noreply.github.com>
2023-07-28 13:28:18 -0400

    Add chrome

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 docker build -t bandi13/gui-docker:latest -f Dockerfile .
 docker build -t bandi13/gui-docker:firefox -f Dockerfile.firefox .
+docker build -t bandi13/gui-docker:chrome -f Dockerfile.chrome .
